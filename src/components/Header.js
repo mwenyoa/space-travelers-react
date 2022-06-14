50fac8fc3f6c42fbcba55f1d7da524dcd3d44884
Tugsem <saranlitugsem@gmail.com>
2022-06-14 13:56:11 +0300

    Add style to the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,18 +3,18 @@ import { NavLink } from 'react-router-dom';
 import logo from './assets/spacex.png';
 
 const Header = () => (
-  <header>
-    <div className="logo">
-      <img src={logo} alt="spacex logo" />
+  <header className="d-flex">
+    <div className="d-flex logo-title">
+      <img className="logo" src={logo} alt="spacex logo" />
       <h1>Space Travelers&apos; Hub</h1>
     </div>
-    <nav>
+    <nav className="d-flex nav-links">
       <NavLink to="/Rockets">Rockets</NavLink>
       <NavLink to="/Missions">Missions</NavLink>
+      <div className="line" />
       <NavLink to="/MyProfile">My Profile</NavLink>
     </nav>
   </header>
-
 );
 
 export default Header;
