13dd858051a8280e5b17d4b9d4dbdc400592fb4b
Tugsem <saranlitugsem@gmail.com>
2022-06-14 17:34:55 +0300

    Add redux folder for all state slice files

---
+++ src/Redux/missions/missionReducer.js
@@ -0,0 +1,32 @@
+import getData from './fetchAPI';
+
+export const ADD_STORE = 'ADD_STORE';
+
+export const addStore = (payload) => ({
+  type: ADD_STORE,
+  payload,
+});
+
+export const displayMissions = () => async (dispatch) => {
+  const missions = await getData();
+  const arr = [];
+  missions.forEach((obj) => {
+    arr.push({
+      mission_id: obj.mission_id,
+      mission_name: obj.mission_name,
+      description: obj.description,
+    });
+  });
+  dispatch(addStore(arr));
+};
+
+const missionReducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_STORE:
+      return [...state, action.payload];
+    default:
+      return state;
+  }
+};
+
+export default missionReducer;
