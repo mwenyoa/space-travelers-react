ef32db6ed73a823ebcb2b051ee74a4a30117f908
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-06-15 14:37:47 +0200

    Fix linter errors

--- src/Redux/missions/fetchAPI.js
+++ src/Redux/missions/fetchAPI.js
@@ -3,7 +3,8 @@ const url = 'https://api.spacexdata.com/v3/missions';
 const getData = async () => {
   const res = (await fetch(url)) || [];
   const data = res.json();
-  console.log(data);
   return data;
 };
+
+getData();
 export default getData;
