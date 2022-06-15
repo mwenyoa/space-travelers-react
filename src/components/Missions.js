3a3cd56c66baad9d80f3310dba506facc88f2020
Tugsem <saranlitugsem@gmail.com>
2022-06-15 14:10:35 +0300

    Add useEffect hook to call dispatch function

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,31 +1,20 @@
-import React, { useCallback } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import getData from '../Redux/missions/fetchAPI';
-import { addStore } from '../Redux/missions/missionReducer';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { displayMissions } from '../Redux/missions/missionReducer';
 
-const displayMissions = () => async (dispatch) => {
-  const missions = await getData();
-  const arr = [];
-  missions.forEach((obj) => {
-    arr.push({
-      mission_id: obj.mission_id,
-      mission_name: obj.mission_name,
-      description: obj.description,
-    });
-  });
-  dispatch(addStore(arr));
-  return arr;
-};
+let triggerOnce = false;
 
 const Missions = () => {
-  const missionList = useSelector((state) => state.missionReducer);
   const dispatch = useDispatch();
 
-  useCallback(() => {
+  useEffect(() => {
+    if (triggerOnce) return;
     dispatch(displayMissions());
+    triggerOnce = true;
   }, []);
 
-  console.log(missionList);
-  return <div>Hello</div>;
+  return (
+    <div>Hello</div>
+  );
 };
 export default Missions;
