6017f15f991669a84da520e4d2f36afcce1a9ce8
Tugsem <saranlitugsem@gmail.com>
2022-06-15 17:49:51 +0300

    Change directory path

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Rockets from './components/Rockets';
-import Missions from './components/Missions';
+import Missions from './components/Missions/Missions';
 import Profile from './components/Profile';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Rockets />} />
+      <Route exact path="/" element={<Rockets />} />
       <Route path="Missions" element={<Missions />} />
       <Route path="Profile" element={<Profile />} />
     </Routes>
