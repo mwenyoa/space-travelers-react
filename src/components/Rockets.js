6b9cd42cc09a3c3bed8f78be920fa492b6097e37
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-06-17 01:28:54 +0200

    Render rockets information to the UI

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,17 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchRockets } from '../Redux/rockets/rocketsReducer';
+import Rocket from './Rocket';
 
-const Rockets = () => (
-  <>
-    <section className="rocket-details d-flex">
-      <div className="rocket-img-sec"><img src="" alt="Rocket-img" /></div>
-      <div className="reserve-rocket-info d-flex flex-d-column">
-        <h2>Rocket Name</h2>
-        <div className="rocket-desc"><p>Hello World from spacex</p></div>
-        <div className="reserve-btn">
-          <button type="button">Reserve Rocket</button>
-        </div>
-      </div>
-    </section>
-  </>
-);
+const Rockets = () => {
+  const rockets = useSelector((state) => state.rockets.rockets);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, [dispatch]);
+
+  return rockets.map((rocket) => <Rocket key={rocket.id} rocket={rocket} />);
+};
+
 export default Rockets;
