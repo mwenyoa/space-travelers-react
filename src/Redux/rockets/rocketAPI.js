c3206d9ba9bad2713db0e1eceea49726b45299aa
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-06-17 01:31:24 +0200

    Fix  linter errors and install propTypes npm dependencie

--- src/Redux/rockets/rocketAPI.js
+++
@@ -1,9 +0,0 @@
-const URL = 'https://api.spacexdata.com/v3/rockets';
-
-const fetchRocketData = async () => {
-  const response = (await fetch(URL)) || [];
-  const resRockets = response.json();
-  return resRockets;
-};
-
-export default fetchRocketData;
