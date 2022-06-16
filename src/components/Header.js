97b1c2826dcf5371d47b669ba52c3028dab08c6f
Tugsem <saranlitugsem@gmail.com>
2022-06-16 19:26:08 +0300

    Add list of reserved missions from filtered array

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => (
   <header className="d-flex">
     <div className="d-flex logo-title">
       <img className="logo" src={logo} alt="spacex logo" />
-      <h1>Space Travelers&apos; Hub</h1>
+      <h1><b>Space Travelers&apos; Hub</b></h1>
     </div>
     <nav className="d-flex nav-links">
       <NavLink to="/">Rockets</NavLink>
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { ListGroup, Col, Row } from 'react-bootstrap';
 
-const Profile = () => (
-  <>
-    <div className="rockets-pr" />
-    <div className="mission-pr" />
-
-  </>
-);
+const Profile = () => {
+  const missions = useSelector((state) => state.missionReducer);
+  const reservedMissions = missions.filter((mission) => mission.reserved === true);
+  return (
+    <Row>
+      <Col className="mission-pr p-5">
+        <h2><b>My Missions</b></h2>
+        <ListGroup className="w-100 mx-auto">
+          {reservedMissions.map((mission) => (
+            <ListGroup.Item key={mission.id}>{mission.name}</ListGroup.Item>
+          ))}
+        </ListGroup>
+      </Col>
+      <Col className="rockets-pr p-5">
+        <h2><b>My Rockets</b></h2>
+        <ListGroup />
+      </Col>
+    </Row>
+  );
+};
 
 export default Profile;
