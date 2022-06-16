1723d1adedba1f0705ae985853b4eb86e3b7755a
Tugsem <saranlitugsem@gmail.com>
2022-06-16 19:32:10 +0300

    Add list of reserved missions from filtered array

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { ListGroup, Col, Row } from 'react-bootstrap';
+import React from "react";
+import { useSelector } from "react-redux";
+import { ListGroup, Col, Row } from "react-bootstrap";
 
 const Profile = () => {
   const missions = useSelector((state) => state.missionReducer);
-  const reservedMissions = missions.filter((mission) => mission.reserved === true);
+  const reservedMissions = missions.filter(
+    (mission) => mission.reserved === true
+  );
   return (
     <Row>
       <Col className="mission-pr p-5">
-        <h2><b>My Missions</b></h2>
+        <h2>
+          <b>My Missions</b>
+        </h2>
         <ListGroup className="w-100 mx-auto">
           {reservedMissions.map((mission) => (
             <ListGroup.Item key={mission.id}>{mission.name}</ListGroup.Item>
@@ -16,7 +20,9 @@ const Profile = () => {
         </ListGroup>
       </Col>
       <Col className="rockets-pr p-5">
-        <h2><b>My Rockets</b></h2>
+        <h2>
+          <b>My Rockets</b>
+        </h2>
         <ListGroup />
       </Col>
     </Row>
