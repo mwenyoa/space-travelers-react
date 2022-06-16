85dc639205d096288853e03996d057ec48a08311
Tugsem <saranlitugsem@gmail.com>
2022-06-16 14:16:39 +0300

    Add new action for the join button which add new 'reserved: true' to the mission with id

--- src/Redux/missions/missionReducer.js
+++ src/Redux/missions/missionReducer.js
@@ -1,10 +1,16 @@
 import getData from './fetchAPI';
 
 export const ADD_STORE = 'space-travelers-react/Redux/ADD_STORE';
+export const JOIN_MISSION = 'space-travelers-react/Redux/JOIN_MISSION';
 
 export const addStore = (payload) => ({
   type: ADD_STORE,
   payload,
+});
+
+export const reserveMission = (id) => ({
+  type: JOIN_MISSION,
+  id,
 });
 
 export const displayMissions = () => async (dispatch) => {
@@ -19,11 +25,19 @@ export const displayMissions = () => async (dispatch) => {
   });
   dispatch(addStore(arr));
 };
+export const handleJoinButton = (id) => (dispatch) => {
+  dispatch(reserveMission(id));
+};
 
 const missionReducer = (state = [], action) => {
   switch (action.type) {
     case ADD_STORE:
       return [...state, action.payload];
+    case JOIN_MISSION:
+      return state.flat().map((mission) => {
+        if (mission.id !== action.id) return mission;
+        return { ...mission, reserved: true };
+      });
     default:
       return state;
   }
