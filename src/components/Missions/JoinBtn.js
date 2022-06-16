6cc0a83316a064d42e85c69695b18e33c4cfce51
Tugsem <saranlitugsem@gmail.com>
2022-06-16 18:30:05 +0300

    Add new action to cancel reservation and displayed on the UI

---
+++ src/components/Missions/JoinBtn.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Button } from 'react-bootstrap';
+import propTypes from 'prop-types';
+import {
+  handleJoinButton,
+  handleLeaveButton,
+} from '../../Redux/missions/missionReducer';
+// import store from '../../Redux/ConfigureStore';
+
+const JoinBtn = ({ id, reserved }) => {
+  const dispatch = useDispatch();
+  let button;
+  if (!reserved) {
+    button = (
+      <Button
+        variant="outline-secondary"
+        type="Button"
+        className="text-nowrap"
+        onClick={() => dispatch(handleJoinButton(id))}
+      >
+        Join Mission
+      </Button>
+    );
+  } else {
+    button = (
+      <Button
+        variant="outline-danger"
+        type="Button"
+        className="text-nowrap"
+        onClick={() => dispatch(handleLeaveButton(id))}
+      >
+        Leave Mission
+      </Button>
+    );
+  }
+  return (
+    <div>
+      {button}
+    </div>
+  );
+};
+
+JoinBtn.propTypes = {
+  id: propTypes.string.isRequired,
+  reserved: propTypes.bool.isRequired,
+};
+
+export default JoinBtn;
